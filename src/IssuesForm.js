3271d48868f27ab8ab2ec47ef759fe10286a4a72
Cyberia 2045 <sagansociety86@gmail.com>
2017-06-09 16:25:49 -0400

    temporary hold on the maps branch

---
+++ src/IssuesForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class IssuesForm {
+
+	render() {
+
+		let neighborhoods = this.neighborhoods.map(function(neighborhood, index) {
+
+		constructor(props) {
+			super(props)
+		}
+
+		return(
+				<div className="issues-form-container">
+					<select name="category">
+						<option value= >
+					</select>
+					<select name="neighborhoods">
+						<option value= >
+					</select>
+					<input type="text" placeholder="description" />
+				</div>
+			)
+
+	}
+
+})
+
+}
+
+export default IssuesForm;
